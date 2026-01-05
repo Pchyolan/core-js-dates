a70216567511f8f843843576bce156861e4b1b50
unknown <mozhno_prost_kostya@mail.ru>
2026-01-06 02:59:33 +0300

    feat: do task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -190,8 +190,17 @@ function formatDate(date) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const date = new Date(year, month - 1, 1);
+  let weekends = 0;
+  while (date.getMonth() === month - 1) {
+    const day = date.getDay();
+    if (day === 0 || day === 6) {
+      weekends += 1;
+    }
+    date.setDate(date.getDate() + 1);
+  }
+  return weekends;
 }
 
 /**
